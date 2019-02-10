24747430c5e3dee040e94af313704bddcc52e8fc
Kumaraditya03 <40833579+Kumaraditya03@users.noreply.github.com>
2019-02-10 08:14:15 +0530

    Update desc_order_codewars.py

--- desc_order_codewars.py
+++ desc_order_codewars.py
@@ -4,8 +4,10 @@ def desc_order(num):
         li.append(i)
     li.sort()
     a=li[::-1]
-    b=int(''.join(map(str,a)))
+    b=int(''.join(map(str,a))) #returns an integer using list members
     print(b)
     print(type(b))
 a=int(input("Enter the number to be ordered:"))
-desc_order(a)
+desc_order(a)            
+#Sample i/p:123456789
+#Sample o/p:987654321
